1b4d03ffe6fa634adf33e9f9a37f7595455108b3
mkattus <mark@mesarim.com>
2020-04-16 12:31:18 -0700

    Clarify overall description.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## The following functions, when used together, will take a square, invertible matrix, calculate 
-## its inverse, and cache the inverse. Caching the matrix avoids having to recalculate the inverse
-## (a costly computation) if the inverse is called after the initial calculation.       
+## its inverse, and cache the inverse. Caching the inverted matrix avoids having to recalculate the 
+## inverse (a costly computation) if the inverse is called after the initial calculation.       
 
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 ## This function builds a set of functions and returns them in a list. These functions are 
